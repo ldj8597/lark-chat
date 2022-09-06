907ccaf43f3991b46ed572a1e676540b8d7b68bf
larkascending <ldj8597@gmail.com>
2022-09-06 23:42:28 +0900

    add websocketserver

--- src/server/context.ts
+++ src/server/context.ts
@@ -2,21 +2,28 @@ import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { prisma } from "./db/client";
 import { authOptions } from "../pages/api/auth/[...nextauth]";
-import { unstable_getServerSession } from "next-auth";
+// import { unstable_getServerSession as getSession } from "next-auth";
+import { getSession } from "next-auth/react";
+import { NodeHTTPCreateContextFnOptions } from "@trpc/server/adapters/node-http";
+import { IncomingMessage } from "http";
+import ws from "ws";
 
-export async function createContext({
+export const createContext = async ({
   req,
   res,
-}: trpcNext.CreateNextContextOptions) {
-  const session = await unstable_getServerSession(req, res, authOptions);
-
+}:
+  | trpcNext.CreateNextContextOptions
+  | NodeHTTPCreateContextFnOptions<IncomingMessage, ws>) => {
+  const session = await getSession({ req });
+  console.log("createContext for", session?.user?.name ?? "unknown user");
   return {
     req,
     res,
     prisma,
     session,
   };
-}
+};
+
 type Context = trpc.inferAsyncReturnType<typeof createContext>;
 
 // Helper function to create a router with your app's context
